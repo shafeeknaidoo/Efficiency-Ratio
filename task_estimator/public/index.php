e13eb5e196bdbdc09ba229ade2bc432b045eaf00
Shafeek Naidoo <shafeekn@gmail.com>
2015-04-22 11:13:30 +0200

    RSA-3757 new class name and extra function for tester

--- task_estimator/public/index.php
+++ task_estimator/public/index.php
@@ -15,7 +15,7 @@ set_include_path(implode(PATH_SEPARATOR, array(
 )));
 
 /** Zend_Application */
-require_once 'Zend/Application.php';
+require_once 'Zend/Applications.php';
 
 // Create application, bootstrap, and run
 $application = new Zend_Application(
